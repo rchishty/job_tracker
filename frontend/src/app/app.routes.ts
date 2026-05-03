b85286878311134369e6808953568843a1d9e49e
rchishty <raynad36@gmail.com>
2026-05-03 19:01:36 -0400

    Changed initial redirect to /applications

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { ReportComponent } from './components/report/report';
 import { ApplicationDetailComponent } from './components/application-detail/application-detail';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'applications', pathMatch: 'full' },
+  { path: '', redirectTo: '/applications', pathMatch: 'full' },
   { path: 'applications', component: ApplicationsComponent },
   { path: 'applications/:id', component: ApplicationDetailComponent },
   { path: 'report', component: ReportComponent }
